4405310ec862df1b2512109cc42282ac515479e7
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2024-11-19 14:29:07 +0100

    laika code fixed

--- src/components/Challenges/FindLaika/FindLaika.jsx
+++ src/components/Challenges/FindLaika/FindLaika.jsx
@@ -1,13 +1,28 @@
-const planet = {
-  image: "https://images.unsplash.com/photo-1560807707-9b1b5e07d51d",
-};
+import { createSignal, onMount } from "solid-js";
 
-return (
-  <div className="container">
-    <h1>Find Laika</h1>
-    <img src={planet.image} alt="Planet" />
-    <div className="planet" draggable="true"></div>
-  </div>
-);
+const FindLaika = () => {
+  const [planet, setPlanet] = createSignal(null);
+
+  onMount(() => {
+    // Simulating fetching data (if dynamic data is needed)
+    setPlanet({
+      image: "https://images.unsplash.com/photo-1560807707-9b1b5e07d51d",
+    });
+  });
+
+  return (
+    <div className="container">
+      <h1>Find Laika</h1>
+      {planet() ? (
+        <>
+          <img src={planet().image} alt="Planet" />
+          <div className="planet" draggable="true"></div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
 export default FindLaika;
