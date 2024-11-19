0d8392762ff662a93f580165a4cddd1456f19e4c
gdmgent-siebedegelas-2004 <siebe.degelas@student.arteveldehs.be>
2024-11-19 11:41:23 +0100

    edit login and navbar logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,26 +4,30 @@ import { A, createAsync } from "@solidjs/router";
 import Container from "../Container";
 
 const NavBar = () => {
-	const user = createAsync(async () => getUser(), { deferStream: true });
-	return (
-		<>
-			{user() && (
-				<div class="wrapper">
-					<Container>
-						<form action={logout} method="post">
-							<A href="/">
-								<img class="logo" src="../../logo_htf.webp" alt="Space Cadet Program" />
-							</A>
-							<A href="/profile">Hello {user()!.username} 👋</A>
-							<button name="logout" type="submit">
-								Logout
-							</button>
-						</form>
-					</Container>
-				</div>
-			)}
-		</>
-	);
+  const user = createAsync(async () => getUser(), { deferStream: true });
+  return (
+    <>
+      {user() && (
+        <div class="wrapper">
+          <Container>
+            <form action={logout} method="post">
+              <A href="/">
+                <img
+                  class="logo"
+                  src="../../9_elixir.png"
+                  alt="Space Cadet Program"
+                />
+              </A>
+              <A href="/profile">Hello {user()!.username} 👋</A>
+              <button name="logout" type="submit">
+                Logout
+              </button>
+            </form>
+          </Container>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default NavBar;
